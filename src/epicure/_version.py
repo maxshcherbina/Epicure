a123cdac51c9f007d1f9f7bff7f6bfad2dc9c566
Gaelle Letort <gaelle.letort.gl@gmail.com>
2026-05-13 14:48:47 +0200

    adding setuptools dep in pixi file

--- src/epicure/_version.py
+++ src/epicure/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev193+g631d088.d20260324'
-__version_tuple__ = version_tuple = (0, 1, 'dev193', 'g631d088.d20260324')
+__version__ = version = '1.5.4.dev47+g2d83614.d20260513'
+__version_tuple__ = version_tuple = (1, 5, 4, 'dev47', 'g2d83614.d20260513')
